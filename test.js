d3a15706bc4871cd5fa4b55c5638d312371b2761
liuwenjin <liuwenjin@jspp.com>
2024-07-17 23:44:50 +0800

    feat: async await

--- test.js
+++ test.js
@@ -1,7 +1,25 @@
-var arr = [];
-for (var i = 0; i < 10; i++) {
-    arr[i] = function() {
-        console.log(i)
-    }
+async function f() {
+    let promise = new Promise(
+        (resolve, reject) => {setTimeout(() => resolve('done'), 1000)});
+    let result = await promise;
+    console.log(result);
 }
-arr[0]()
+
+
+f();
+//
+//
+//
+// async function f() {
+//     return Promise.resolve(1);
+// }
+// f().then(console.log);
+
+
+// var arr = [];
+// for (var i = 0; i < 10; i++) {
+//     arr[i] = function() {
+//         console.log(i)
+//     }
+// }
+// arr[0]()
--- test.rs
+++ test.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("value: {}", 0.1 + 0.2);
+    // 创建两个新线程执行任务
+    let thread_one = thread::spawn(|| download("https://course.rs"));
+    let thread_two = thread::spawn(|| download("https://fancy.rs"));
+
+    // 等待两个线程的完成
+    thread_one.join().expect("thread one panicked");
+    thread_two.join().expect("thread two panicked");
+
+    //    println!("{}", 0.1 + 0.2);
 }
