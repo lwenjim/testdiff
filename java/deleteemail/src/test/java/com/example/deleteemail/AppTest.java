d38afefba849a924b642b18d417ce1eb82222999
liuwenjin <liuwenjin@jspp.cn>
2024-10-12 19:05:01 +0800

    feat: update delete rule

--- java/deleteemail/src/test/java/com/example/deleteemail/AppTest.java
+++ java/deleteemail/src/test/java/com/example/deleteemail/AppTest.java
@@ -23,7 +23,6 @@ public class AppTest {
         Store store = EmailTest.getStore();
         Folder folder = store.getFolder("inbox");
         int step = 10;
-        int threadNum = 4;
         while (true) {
             try {
                 if (!folder.isOpen()) {
@@ -33,25 +32,17 @@ public class AppTest {
                 if (totalCount <= 0) {
                     break;
                 }
-                int w = totalCount - step * threadNum;
-                if (EmailTest.getMessages(folder, w, totalCount).length == 0) {
+                Message[] messages = EmailTest.getMessages(folder, totalCount - step * 12, totalCount);
+                if (messages.length == 0) {
                     break;
                 }
-                Thread[] list = new Thread[threadNum];
-                for (int j = 0; j < threadNum; j++) {
+                Thread[] list = new Thread[12];
+                for (int j = 0; j < 12; j++) {
                     final int start = j;
-                    final int begin = w;
                     Thread t = new Thread(new Runnable() {
                         public void run() {
                             try {
-                                Store store1 = EmailTest.getStore();
-                                Folder folder1 = store1.getFolder("inbox");
-                                if (!folder1.isOpen()) {
-                                    folder1.open(Folder.READ_WRITE);
-                                }
-                                Message[] messages = EmailTest.getMessages(folder, begin + start * step,
-                                        begin + (start + 1) * step -1);
-                                for (int i = 0; i < messages.length; i++) {
+                                for (int i = start * step; i < (start + 1) * step; i++) {
                                     String subject = messages[i].getSubject();
                                     String from = (messages[i].getFrom()[0]).toString();
                                     System.out.println(from);
@@ -61,16 +52,14 @@ public class AppTest {
                                     System.out.printf(
                                             "\n第 %d 封邮件\n标题: %s\n发件人：%s\n邮件总数: %d\n未读邮件数: " + "%d\n邮件是否已读: "
                                                     + "%s\n发送时间：%s\n邮件优先级：%s\n邮件大小：%skb\n",
-                                            i + 1, subject, from, folder1.getMessageCount(),
-                                            folder1.getUnreadMessageCount(),
+                                            i + 1, subject, from, folder.getMessageCount(),
+                                            folder.getUnreadMessageCount(),
                                             messages[i].getFlags().contains(Flags.Flag.SEEN) ? "是" : "否",
                                             EmailTest.getSentDate((MimeMessage) messages[i], null),
                                             EmailTest.getPriority((MimeMessage) messages[i]),
                                             ((MimeMessage) messages[i]).getSize() * 1024);
                                     messages[i].setFlag(Flags.Flag.DELETED, true);
                                 }
-                                folder1.expunge();
-                                store1.close();
                             } catch (Exception e) {
                                 System.out.println("异常： " + e);
                             }
@@ -82,7 +71,7 @@ public class AppTest {
                 for (Runnable r : list) {
                     r.wait();
                 }
-                System.out.printf("已删除 %d 封邮件\n", step * 12);
+                folder.expunge();
             } catch (Exception e) {
                 System.out.println("异常： " + e);
             }
